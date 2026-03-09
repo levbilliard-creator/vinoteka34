73cf02a11ff7f7cce365fd683893142eac508fc9
levbilliard-creator <levbilliard@gmail.com>
2026-03-09 18:59:50 +0300

    Add files via upload

--- product.js
+++ product.js
@@ -13,11 +13,11 @@ const product = products.find(p => p.id == id);
 const container = document.getElementById("product-info");
 
 if(!product){
-container.innerHTML = "Вино не найдено";
+container.innerHTML="Вино не найдено";
 return;
 }
 
-container.innerHTML = `
+container.innerHTML=`
 
 <h1 class="product-title">
 ${product.name}
@@ -37,13 +37,13 @@ ${product.price} ₽
 
 `;
 
-const similar = products
-.filter(p => p.type === product.type && p.id != product.id)
+const similar=products
+.filter(p=>p.type===product.type && p.id!=product.id)
 .slice(0,4);
 
-const similarContainer = document.getElementById("similar-products");
+const similarContainer=document.getElementById("similar-products");
 
-similarContainer.innerHTML = similar.map(p => `
+similarContainer.innerHTML=similar.map(p=>`
 
 <div class="product-card">
 
