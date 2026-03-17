400536bbae22ef5a422a2ad1dec57599d91ab50e
levbilliard-creator <levbilliard@gmail.com>
2026-03-17 14:38:26 +0300

    Add files via upload

--- product.js
+++ product.js
@@ -1,7 +1,7 @@
-let ALL = []
-
-const params = new URLSearchParams(window.location.search)
+const params = new URLSearchParams(location.search)
 const id = Number(params.get("id"))
+
+let ALL = []
 
 init()
 
@@ -12,94 +12,49 @@ async function init(){
 
   const wine = ALL.find(w => w.id === id)
 
-  if(!wine){
-    document.body.innerHTML = "Товар не найден"
-    return
-  }
-
-  render(wine)
+  renderProduct(wine)
   renderSimilar(wine)
 
 }
 
 
-/* ===== ОСНОВНОЙ РЕНДЕР ===== */
+/* ТОВАР */
 
-function render(w){
+function renderProduct(w){
 
-  document.querySelector(".productTitle").innerText = w.name_ru
-
-  document.querySelector(".wine-img-big").src =
-    w.image || "./assets/no-wine.png"
-
-  document.querySelector(".wine-price-big").innerText =
-    w.price + " ₽"
-
-  const info = document.querySelector(".productInfo")
-
-  info.innerHTML += `
-    <div class="wine-type">${translate(w.type)}</div>
-
-    <div class="wine-en">${w.name_en || ""}</div>
-
-    <div class="wine-style">
-      ${w.color || ""} ${w.sweetness || ""}
+  document.querySelector(".productWrapper").innerHTML = `
+    <div class="productImage">
+      <img src="${w.image || './assets/no-wine.png'}" class="wine-img-big">
     </div>
 
-    <div class="wine-description">
-      Идеально подходит для ужина, сочетается с мясом, сырами и закусками.
+    <div class="productInfo">
+      <div class="wine-type">${w.type}</div>
+      <h1>${w.name_ru}</h1>
+      <div>${w.color || ""} ${w.sweetness || ""}</div>
+
+      <div class="wine-price-big">${w.price} ₽</div>
+
+      <button class="wine-btn">Спросить сомелье</button>
     </div>
   `
-
 }
 
 
-/* ===== РЕКОМЕНДАЦИИ ===== */
+/* РЕКОМЕНДАЦИИ */
 
-function renderSimilar(current){
+function renderSimilar(w){
 
-  const container = document.querySelector(".similarGrid")
-
-  if(!container) return
-
-  const items = ALL
-    .filter(w => w.type === current.type && w.id !== current.id)
+  const similar = ALL
+    .filter(x => x.type === w.type && x.id !== w.id)
     .slice(0,4)
 
-  container.innerHTML = ""
-
-  items.forEach(w => {
-
-    container.innerHTML += `
-      <div class="product-card">
-
-        <div class="image-wrap">
-          <img src="${w.image || './assets/no-wine.png'}" class="wine-img">
-        </div>
-
-        <div class="wine-ru">${w.name_ru}</div>
-
-        <div class="wine-price">${w.price} ₽</div>
-
-        <a href="./product.html?id=${w.id}" class="btn-link">
-          Подробнее →
-        </a>
-
-      </div>
-    `
-  })
-
-}
-
-
-/* ===== ПЕРЕВОД ===== */
-
-function translate(type){
-
-  if(type === "wine") return "Вино"
-  if(type === "sparkling") return "Игристое"
-  if(type === "strong") return "Крепкий алкоголь"
-  if(type === "beer") return "Пиво"
-
-  return "Напиток"
+  document.querySelector(".recommendGrid").innerHTML = similar.map(x => `
+    <div class="product-card">
+      <img src="${x.image || './assets/no-wine.png'}" class="wine-img">
+      <div class="wine-type">${x.type}</div>
+      <div class="wine-ru">${x.name_ru}</div>
+      <div class="wine-price">${x.price} ₽</div>
+      <a href="./product.html?id=${x.id}" class="btn-link">Подробнее</a>
+    </div>
+  `).join("")
 }
